6a8a289e16b558a83a84c0bb19c02606460d11c5
Jiawei <liujiaweidreamer@gmail.com>
2020-08-23 18:41:58 +0800

    函数式组件的参数传递方法

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // react
 {
   let number = 0;
+  const age = 18;
 
-  const App = () => {
+  const App = (props) => {
     return (
       <div className="app" id="virtual-dom">
         <p>React</p>
+        <p>传给我的 name 是：{props.name}</p>
+        <p>传给我的 age 是：{props.age}</p>
         <span>{number}</span>
         <button className="button" id="add" onClick={add}>+1</button>
         <button className="button" id="minus" onClick={minus}>-1</button>
@@ -16,7 +19,7 @@
   const render = () => {
     // create virtual DOM and render it to real DOM
     ReactDOM.render(
-      <App/>,
+      <App name="刘二男" age={age}/>,
       document.querySelector("#react-app"));
   };
 
