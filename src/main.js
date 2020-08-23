490f6817249fd3e0ab246ea4a55007585c7f083d
Jiawei <liujiaweidreamer@gmail.com>
2020-08-23 14:17:29 +0800

    先用原生 JS 实现一个计数器

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log(React)
+// plain JavaScript
+const $output = document.querySelector(".output");
+const $addButton = document.querySelector("#add");
+const $minusButton = document.querySelector("#minus");
+
+$addButton.addEventListener("click", () => {
+  const value = parseInt($output.innerText, 10);
+  $output.innerText = (value + 1).toString();
+});
+
+$minusButton.addEventListener("click", () => {
+  const value = parseInt($output.innerText, 10);
+  $output.innerText = (value - 1).toString();
+});
